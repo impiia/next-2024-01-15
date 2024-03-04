77f4358aac59c853ead150d968f027817c2be336
impiia <impiia28@gmail.com>
2024-03-04 14:23:26 +0400

    added isActive to tabs

--- src/components/control-tabs/component.jsx
+++ src/components/control-tabs/component.jsx
@@ -1,19 +1,24 @@
-
+'use client'
 import Link from 'next/link';
 import { Button } from '../button/component';
 import styles from './styles.module.scss';
+import { usePathname } from 'next/navigation'
 
 export const ControlTabs = ({ restaurantId }) => {
+    const pathname = usePathname();
+    const isActiveMenu = pathname.endsWith("/menu");
+    const isActiveReview = pathname.endsWith("/reviews");
+
     return (
         <div >
-
+        
             <Link href={`/restaurants/${restaurantId}/menu`} className={styles.pageLink}>
-                <Button>Menu</Button>
+                <Button disabled={isActiveMenu}>Menu</Button>
             </Link>
 
 
             <Link href={`/restaurants/${restaurantId}/reviews`} className={styles.pageLink}>
-                <Button>Review</Button>
+                <Button disabled={isActiveReview}>Review</Button>
             </Link>
         </div>
     );
--- src/components/tabs/components.jsx
+++ src/components/tabs/components.jsx
@@ -1,15 +1,25 @@
+
+'use client'
 import Link from 'next/link';
 import styles from './styles.module.scss';
 import { Button } from '../button/component';
+import { usePathname } from 'next/navigation'
 
 export const Tabs = ({ restaurants }) => {
+    const pathname = usePathname();
     return (
         <div>
-            {restaurants.map((restaurant) => (
-                <Link href={`/restaurants/${restaurant.id}`} key={restaurant.id} className={styles.pageLink}>
-                    <Button>{restaurant.name}</Button>
-                </Link>
-            ))}
+             {restaurants.map((restaurant) => {
+                const id = restaurant.id.toString();
+                const isActive = pathname.endsWith(id);
+
+                return (
+                    <Link href={`/restaurants/${restaurant.id}`} key={restaurant.id} passHref
+                        className={styles.pageLink}>
+                            <Button disabled={isActive}>{restaurant.name}</Button>
+                    </Link>
+                );
+            })}
         </div>
     );
 };
